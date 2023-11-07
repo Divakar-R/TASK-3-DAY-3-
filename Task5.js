55d04451a17b06d1e36815380352426266f0a1e9
Divakar-R <148629037+Divakar-R@users.noreply.github.com>
2023-11-07 22:15:31 +0530

    Task5.js

---
+++ Task5.js
@@ -0,0 +1,13 @@
+PROGRAMS USING ANONYMOUS FUNCTION:
+
+//1.Print odd numbers in an array
+
+   const numbers =[1,2,3,4,5,6,7,8,9,10];
+numbers.forEach(function(oddnumber){
+    if(oddnumber%2!==0){
+        console.log(oddnumber);     
+    }
+});
+
+//2.Convert all the strings to title caps in a string array
+
