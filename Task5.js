6c3ad0084bec892cc9f7b63f0fc0feb9d5fda9ed
Divakar-R <148629037+Divakar-R@users.noreply.github.com>
2023-11-08 07:45:31 +0530

    Update Task5.js

--- Task5.js
+++ Task5.js
@@ -28,3 +28,22 @@ numbers.forEach(function(item) {
 });
 
 console.log(sum);
+
+
+
+//4.Return all the prime numbers in an array
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const primeNumbers = numbers.filter(function(number) {
+  if (number <= 1) return false;
+  if (number <= 3) return true;
+  if (number % 2 === 0 || number % 3 === 0) return false;
+  
+   for (let i = 5; i * i <= number; i += 6) {
+    if (number % i === 0 || number % (i + 2) === 0) return false;
+  }
+    return true;
+});
+console.log(primeNumbers);
