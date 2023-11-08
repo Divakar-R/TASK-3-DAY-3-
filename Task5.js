87e691d0de6137789875350800fb7c53f43542b4
Divakar-R <148629037+Divakar-R@users.noreply.github.com>
2023-11-08 07:23:35 +0530

    Update Task5.js

--- Task5.js
+++ Task5.js
@@ -11,3 +11,20 @@ numbers.forEach(function(oddnumber){
 
 //2.Convert all the strings to title caps in a string array
 
+
+
+
+
+
+
+
+//3.Sum of all numbers in an array
+
+const numbers = [1, 2, 3, 4, 5];
+let sum = 0;
+
+numbers.forEach(function(item) {
+  sum += item;
+});
+
+console.log(sum);
